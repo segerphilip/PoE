c4d1acd626f72a5b585c404c87a4bb6d5f0021d4
Philip Seger <seger.philip@gmail.com>
2014-09-09 11:34:02 -0400

    millis used for timing, adding functions

--- Lab 1/Arduino Code/Part1/Part1.ino
+++ Lab 1/Arduino Code/Part1/Part1.ino
@@ -3,6 +3,15 @@ const int ledY = 11;
 const int ledR = 10;
 const int button = 7;
 
+//led states
+int ledGState = LOW;
+int ledYState = LOW;
+int ledRState = LOW;
+
+//timing variables
+long previousMillis = 0;
+long interval = 50;
+
 //button counters
 int buttonPush = 0;
 int buttonState = 0;
@@ -16,111 +25,28 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
   buttonState = digitalRead(button);
-  if (buttonState != buttonLast) {
-    if (buttonState == HIGH) {
-      buttonPush++;
-      if (buttonPush == 15) {
-        buttonPush = 0;
-      }
-      Serial.println(buttonPush);
-    }
-  }
-  buttonLast = buttonState;
   
-  if (buttonPush == 1) {
-    digitalWrite(ledG, HIGH);
+  if(currentMillis - previousMillis > interval) {
+      previousMillis = currentMillis;
+      
+      if(buttonState != buttonLast) {
+        if(buttonState == HIGH) {
+          buttonPush++;
+          if(buttonPush == 3) {
+            buttonPush = 0;
+          }
+        }
+      }
+      buttonLast = buttonState;
+      
+      if(buttonPush == 1) {
+        if(ledGState == LOW)
+          ledGState = HIGH;
+        else
+          ledGState = LOW;
+        digitalWrite(ledG, ledGState);
+      }
   }
-  else if (buttonPush == 2) {
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, HIGH);
-  }
-  else if (buttonPush == 3) {
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, HIGH);
-  }
-  else if (buttonPush == 4) {
-    digitalWrite(ledR, LOW);
-    digitalWrite(ledG, HIGH);
-    delay(50);
-    digitalWrite(ledG, LOW);
-  }
-  else if (buttonPush == 5) {
-    digitalWrite(ledY, HIGH);
-    delay(50);
-    digitalWrite(ledY, LOW);
-  }
-  else if (buttonPush == 6) {
-    digitalWrite(ledR, HIGH);
-    delay(50);
-    digitalWrite(ledR, LOW);
-  }
-  else if (buttonPush == 7) {
-    digitalWrite(ledG, HIGH);
-  }
-  else if (buttonPush == 8) {
-    digitalWrite(ledY, HIGH);
-  }
-  else if (buttonPush == 9) {
-    digitalWrite(ledR, HIGH);
-  }
-  else if (buttonPush == 10) {
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, LOW);
-    digitalWrite(ledG, HIGH);
-    delay(50);
-    digitalWrite(ledG, LOW);
-  }
-  else if (buttonPush == 11) {
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, LOW);
-    delay(50);
-    digitalWrite(ledG, HIGH);
-    digitalWrite(ledY, HIGH);
-  }
-  else if (buttonPush == 12) {
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, LOW);
-    delay(50);
-    digitalWrite(ledG, HIGH);
-    digitalWrite(ledY, HIGH);
-    digitalWrite(ledR, HIGH);
-  }
-  else if (buttonPush == 13) {
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, LOW);
-    delay(50);
-    digitalWrite(ledR, LOW);
-    delay(50);
-    digitalWrite(ledG, HIGH);
-    delay(50);
-    digitalWrite(ledY, HIGH);
-    digitalWrite(ledR, HIGH);
-    delay(50);
-  }
-  else if (buttonPush == 14) {
-    digitalWrite(ledG, HIGH);
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, LOW);
-    delay(100);
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, HIGH);
-    delay(50);
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, HIGH);
-    delay(100);
-    digitalWrite(ledR, LOW);
-    digitalWrite(ledY, HIGH);
-    delay(50);
-    //digitalWrite(ledY, LOW);
-    //digitalWrite(ledG, HIGH);
-    //delay(50);
-  }
-  else {
-    digitalWrite(ledG, LOW);
-    digitalWrite(ledY, LOW);
-    digitalWrite(ledR, LOW);
-  }
-  delay(50);
 }
