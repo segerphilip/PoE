d4b5742dbe6d86cb2b817d89601d2b545c6cfca5
Philip Seger <seger.philip@gmail.com>
2014-09-12 11:33:51 -0400

    blinking lights with proper delay and potentiometer used

--- Lab 1/Arduino Code/Part1/Part1.ino
+++ Lab 1/Arduino Code/Part1/Part1.ino
@@ -2,6 +2,7 @@ const int ledG = 12;
 const int ledY = 11;
 const int ledR = 10;
 const int button = 7;
+const int pin = 5;
 
 //led states
 int ledGState = LOW;
@@ -17,6 +18,9 @@ int buttonPush = 0;
 int buttonState = 0;
 int buttonLast = 0;
 
+//counting!
+int count = 0;
+
 void setup() {
   pinMode(button, INPUT);
   pinMode(ledG, OUTPUT);
@@ -25,6 +29,7 @@ void setup() {
 }
 
 void loop() {
+  interval = analogRead(pin) / 2;
   unsigned long currentMillis = millis();
   buttonState = digitalRead(button);
   
@@ -34,7 +39,7 @@ void loop() {
       if(buttonState != buttonLast) {
         if(buttonState == HIGH) {
           buttonPush++;
-          if(buttonPush == 5) {
+          if(buttonPush == 6) {
             buttonPush = 0;
           }
         }
@@ -78,6 +83,35 @@ void loop() {
         digitalWrite(ledG, ledGState);
         digitalWrite(ledY, ledGState);
         digitalWrite(ledR, ledGState);
+      }
+      if(buttonPush == 5) {
+        if(count == 0) {
+          ledGState = HIGH;
+          ledYState = LOW;
+          ledRState = LOW;
+          count++;
+        }
+        else if(count == 1) {
+          ledGState = LOW;
+          ledYState = HIGH;
+          ledRState = LOW;
+          count++;
+        }
+        else if(count == 2) {
+          ledGState = LOW;
+          ledYState = LOW;
+          ledRState = HIGH;
+          count++;
+        }
+        else if(count == 3) {
+          ledGState = LOW;
+          ledYState = HIGH;
+          ledRState = LOW;
+          count = 0;
+        }
+        digitalWrite(ledG, ledGState);
+        digitalWrite(ledY, ledYState);
+        digitalWrite(ledR, ledRState);
       }
   }
 }
